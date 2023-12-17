a6a4634a8c337386895c5bead9b421947cb4bc21
Damon Marc Rocha II <dmarcr1997@gmail.com>
2023-12-16 19:52:38 -0600

    Setup read and write roles
    Created read and write roles based on query param
    setting up roles in working and so is data transmission to reader
    need to fix data save from writer

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,34 @@ use actix::{Actor, StreamHandler, AsyncContext};
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
 use std::str;
-use rand::Rng; // For generating random numbers
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::collections::HashMap;
 
 // Define your WebSocket actor
-struct TerraSocket;
+struct TerraSocket {
+    role: String,
+    topic: Option<String>,
+    topics_data: HashMap<String, (String, Instant)>,
+}
 
 impl Actor for TerraSocket {
     type Context = ws::WebsocketContext<Self>;
 
     // Actor logic here
     fn started(&mut self, ctx: &mut Self::Context) {
-        // Schedule a task to send random data periodically
-        ctx.run_interval(Duration::from_secs(5), |act, ctx| {
-            // Generate random data
-            let random_data = rand::thread_rng().gen_range(0..100);
-            // Send the random data to the client
-            ctx.text(format!("Periodic random data: {}", random_data));
-        });
+        ctx.text(format!("{}", self.role));
+        if self.role == "reader" {
+            if let Some(topic_name) = self.topic.clone() {
+                // Schedule a task to send random data periodically
+                ctx.run_interval(Duration::from_secs(5), move |act, ctx| {
+                    if let Some((value, _timestamp)) = act.topics_data.get(&topic_name) {
+                        ctx.text(format!("Latest data for {}: {}", topic_name, value));
+                    } else {
+                        ctx.text(format!("NO DATA YET FOR {}", topic_name));
+                    }
+                });
+            } else { ctx.text(format!("NO TOPIC YET")); }
+        }
     }
 }
 
@@ -28,9 +38,12 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for TerraSocket {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, _ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Text(text)) => {
-                println!("Received text message: {}", text);
-                // Handle text message
-            
+                if self.role == "writer" {
+                    // Update topic data
+                    if let Some(topic_name) = &self.topic {
+                        self.topics_data.insert(topic_name.clone(), (text.to_string(), Instant::now()));
+                    }
+                }
             }
             Ok(ws::Message::Binary(bin)) => {
                 if let Ok(text) = str::from_utf8(&bin) {
@@ -47,7 +60,21 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for TerraSocket {
 
 // Setup WebSocket route
 async fn websocket_route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    ws::start(TerraSocket, &req, stream)
+    let query_string = req.query_string();
+    let query_params: HashMap<String, String> = url::form_urlencoded::parse(query_string.as_bytes())
+        .into_owned()
+        .collect();
+
+    let role = query_params.get("role").unwrap_or(&String::from("default_role")).to_string();
+    let topic = query_params.get("topic").map(|t| t.to_string());
+
+    let socket = TerraSocket { 
+        role, 
+        topic,
+        topics_data: HashMap::new(),
+    };
+    
+    ws::start(socket, &req, stream)
 }
 
 #[actix_rt::main]
